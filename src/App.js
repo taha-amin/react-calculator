c965247961a4e32435efc77a55c9c4587ff548a4
Mohamed Amin <mohamedamin2009ster@gmail.com>
2023-01-09 14:05:29 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -38,46 +38,46 @@ function App() {
         <div className="btn orange" onClick={operatorType}>
           /
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           7
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           8
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           9
         </div>
         <div className="btn orange" onClick={operatorType}>
           x
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           4
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           5
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           6
         </div>
         <div className="btn orange" onClick={operatorType}>
           +
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           1
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           2
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           3
         </div>
         <div className="btn orange" onClick={operatorType}>
           -
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn zero" onClick={inputNum}>
           0
         </div>
-        <div className="btn light-gray" onClick={inputNum}>
+        <div className="btn" onClick={inputNum}>
           .
         </div>
         <div className="btn orange" onClick={equals}>
